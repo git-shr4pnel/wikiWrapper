02e16731358c8a838dec7f0f2116e5d7a26f6924
shrapnelkun <tdocherty05@gmail.com>
2022-03-04 17:34:44 +0000

    Initial commit

---
+++ WikiWrapper.py
@@ -0,0 +1,57 @@
+import requests
+
+hosts = {
+    "wikipedia": "https://en.wikipedia.org/w/api.php",
+    "wikidata": "https://www.wikidata.com/w/api.php",
+    "wikibooks": "https://en.wikibooks.org/w/api.php"
+}
+responses = {
+    "wikipedia": "https://en.wikipedia.org/?curid=",
+    "wikidata": "https://en.wikidata.org/?curid=",
+    "wikibooks": "https://en.wikibooks.org/?curid="
+}
+
+
+class SearchQuery:
+    def __init__(self, search, host="wikipedia", srnamespace=0, srlimit=10, sroffset=0, srqiprofile="classic",
+                 action="query", call_list="search", rtn_format="json"):
+        self.action = action
+        self.host = host
+        self.call_list = call_list
+        self.search = search
+        self.rtn_format = rtn_format
+        self.search = search
+        self.srnamespace = srnamespace
+        self.srlimit = srlimit
+        self.sroffset = sroffset
+        self.srqiprofile = srqiprofile
+        self.url = None
+        self.__GET()
+
+    def __GET(self):
+        payload = {
+            "action": self.action,
+            "list": self.call_list,
+            "format": self.rtn_format,
+            "srsearch": self.search,
+            "srnamespace": self.srnamespace,
+            "srlimit": self.srlimit,
+            "sroffset": self.sroffset,
+            "srqiprofile": self.srqiprofile
+        }
+        try:
+            r = requests.get(hosts[self.host], params=payload)
+        except KeyError:
+            raise RuntimeError("Invalid host specified")
+        self.url = get_wiki_url(r, self.host)
+        return r
+
+
+def get_wiki_url(pid, response):
+    if type(pid) is not requests.models.Response:
+        raise TypeError("Argument must be a requests object")
+    try:
+        pid = pid.json()["query"]["search"][0]["pageid"]
+    except IndexError:
+        return None
+    return f"{responses[response]}{pid}"
